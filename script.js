5ea4d392a4f6681a1c192dac66adca6f1519bf53
DeepBPatel702 <deepbpatel702@gmail.com>
2021-09-12 21:33:13 +0530

    completed

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+const seatContainer = document.getElementById('screen-container');
+const seats = document.querySelectorAll('.row .seat:not(.sold)');
+const movieSelected = document.getElementById('movie');
+const count = document.getElementById('count')
+const total = document.getElementById('total')
+let ticketPrice = movieSelected.value
+
+const updateSelectedCounts = function(){
+	const selectedSeats = document.querySelectorAll('.row .seat.selected');
+		count.innerText = (selectedSeats.length)
+		total.innerText = ` ₹${selectedSeats.length * ticketPrice}`;
+}
+movieSelected.addEventListener('change', (e) =>{
+	ticketPrice = Number(e.target.value);
+})
+
+seatContainer.addEventListener('click', function(e){
+	if(e.target.classList.contains('seat') && !e.target.classList.contains('sold')){
+		e.target.classList.toggle('selected')
+		updateSelectedCounts();
+	}
+})
+
+updateSelectedCounts()
